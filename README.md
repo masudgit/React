edc7f7d36d564b7c37c4a573c271775c90393b0c
masud.cseu@gmail.com <masud.cseu@gmail.com>
2020-10-10 19:35:17 +0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # React
 React Projects
+
+1. Netflix Clone: https://netflix-clone-6ea6e.web.app/
+
+2. Contact App: https://contact-app-a2818.web.app
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    // if [], run once when the row loads, and don't run again. on page load.
+    // aync, as we are calling a third party api
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  //console.log(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      //https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    console.log("handleCLick");
+    console.log(movie);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          //https://www.youtube.com/watch?v=XtMhy8QK
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      {/* title */}
+      <h2> {title} </h2>
+
+      <div className="row__posters">
+        {/* several row__posters */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"} `}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }
+              `}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* container -> posters */}
+
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
