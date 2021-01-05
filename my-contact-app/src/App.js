250fdbb9e6bd7958a2969f881f4ad1a9c4d90081
masudgit <masud.cseu@gmail.com>
2021-01-05 08:43:19 +0600

    react-html-parser introduced

--- my-contact-app/src/App.js
+++ my-contact-app/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import "./styles.css";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import { Button, Dropdown, Menu } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 const App = () => {
 
   const [contacts, setContacts] = useState([]);
+  const [faculty, setFaculty] = useState([]); 
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=3")
@@ -11,6 +15,14 @@ const App = () => {
       .then(data => {
         console.log(data);
         setContacts(data.results);
+      });
+
+      fetch("http://host:8000/facultyMemberDetails/19")
+      .then(response => response.json())
+      .then(mydata => {
+        console.log(mydata);
+        console.log(mydata);
+        setFaculty(mydata);
       });
 
   }, []);
@@ -26,6 +38,44 @@ const App = () => {
           age={contact.dob.age}
         />
       ))}
+
+    {faculty.map(member => (
+        <MemberCard
+          ResearchInterest={ReactHtmlParser(member.ResearchInterest)}
+        />
+      ))} 
+      
+
+      <Menu size='massive'>
+        <Menu.Item
+          name='home'
+        />
+        <Menu.Item
+          name='messages'
+        />
+
+        <Menu.Menu position='right'>
+          <Dropdown item text='Language'>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                  <Dropdown item text='Style'>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>Russian</Dropdown.Item>   
+                      <Dropdown.Item>Russian</Dropdown.Item> 
+                      <Dropdown.Item>Russian</Dropdown.Item>            
+                    </Dropdown.Menu>
+                  </Dropdown>
+              </Dropdown.Item>
+              <Dropdown.Item>Russian</Dropdown.Item>
+              <Dropdown.Item>Spanish</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Menu.Item>
+            <Button primary>Sign Up</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     </>
   );
 };
@@ -41,6 +91,17 @@ const ContactCard = props => {
         <p>Email: {props.email}</p>
         <button onClick={() => setShowAge(!showAge)}>  Toggle Age </button>
         {showAge ? <p>Age: {props.age}</p> : null}
+      </div>
+    </div>
+  );
+}
+
+const MemberCard = props => {
+  
+  return (
+    <div className="contact-card">
+      <div className="user-details">
+        <p>Name: {props.ResearchInterest}</p>
       </div>
     </div>
   );
