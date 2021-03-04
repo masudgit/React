062fba441bc8eb4b83b829890b9a2da3c8669ec9
masudgit <masud.cseu@gmail.com>
2021-03-04 17:56:03 +0600

    sending email using java-mail-api

---
+++ react-spring/crm/src/main/java/com/mail/SendEmail.java
@@ -0,0 +1,40 @@
+package com.mail;
+
+
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress; 
+import javax.mail.internet.MimeMessage; 
+
+public class SendEmail {
+	public static void main(String [] args){   
+      String to = "masud.ikbal@sonaliintellect.com";//change accordingly  
+      String from = "cbx.retail@sonaliintellect.com";//change accordingly  
+      String host = "smtp01.sonaliintellect.com";//or IP address  
+  
+     //Get the session object  
+      Properties properties = System.getProperties();  
+      properties.setProperty("mail.smtp.host", host);  
+      Session session = Session.getDefaultInstance(properties);   
+  
+     //compose the message  
+      try{  
+         MimeMessage message = new MimeMessage(session);  
+         message.setFrom(new InternetAddress(from));  
+         message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+         message.setSubject("Ping");  
+         message.setText("Hello, this is example of sending email  ");  
+  
+         // Send message  
+         Transport.send(message);  
+         System.out.println("message sent successfully....");  
+  
+      }catch (MessagingException mex) {mex.printStackTrace();}  
+   }
+}
