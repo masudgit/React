3a6bd1a86fea5a756eeb7df6512dd51b991a05a1
masudgit <masud.cseu@gmail.com>
2021-04-07 14:46:24 +0600

    create order

--- react-shopping-cart/server.js
+++ react-shopping-cart/server.js
@@ -70,7 +70,6 @@ app.post("/api/orders", async(req, res) => {
     res.send(order);
 });
 
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("serve at http://localhost:5000"));
 
